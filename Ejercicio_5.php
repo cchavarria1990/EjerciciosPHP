9735d2acf0aaf5a35a89b3849d06b5f98701c5fc
cindy chavarria <cindy.chavarria@unah.hn>
2020-05-31 21:59:34 -0600

    segundo cambio en ciclo while
    prueba segundo cambio en ciclo while

--- Ejercicio_5.php
+++ Ejercicio_5.php
@@ -26,10 +26,11 @@
         echo "<br>";
     }
 
+    echo "<p>ejercicio usando el ciclo While </p>";
+    
     $contador = 0; //variable que se usará para lograr un incremento de los números
-        
+
     while ($contador < count($familia)) { //Extraemos los datos del array de uno en uno mientras haya datos
-           echo ($contador+1).'- '.$familia[$contador].'<br/>'; //Mostramos el contenido del array usando el incremento de la variable
            $contador++; //Usamos la variable para ir incrementando los números
            echo "<h2>$familia[$contador] número de elemento $contador </h2>";
            echo "<br>"; 

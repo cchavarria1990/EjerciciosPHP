41a5a5c5f2a5482904d8566a0cb15c2540107205
cindy chavarria <cindy.chavarria@unah.hn>
2020-06-01 21:41:19 -0600

    mod ejercicio7

--- Ejercicio_7.php
+++ Ejercicio_7.php
@@ -9,13 +9,7 @@ header ("Content-type: text/html;charset=\"utf-8\"");
 if (is_numeric($_GET['numero']) && $_GET['numero'] >1)
 {
     echo "<h3>El valor ".$_GET['numero']." es numerico</h3>";
-}
-else
-{
-    echo "<h3>El valor ".$_GET['numero']." No es numerico</h3>";
-}
-
-if ($_GET['numero']  % 2 != 0) 
+    if ($_GET['numero']  % 2 != 0) 
     {
         for ($i = 1; $i <= $_GET['numero'] ; $i++) 
         {
@@ -29,6 +23,13 @@ if ($_GET['numero']  % 2 != 0)
     {
         echo "<h2>El número ".$_GET['numero']." No es primo</h2>";
     }
+}
+else
+{
+    echo "<h3>El valor ".$_GET['numero']." No es numerico</h3>";
+}
+
+
 
 ?>
 <form>
